e211c3eb8aec93a7964e37727c4a3dfa202796a5
ORLOVSKYJ <amezium1@gmail.com>
2022-06-19 17:18:53 +0300

    init project

---
+++ app/src/main/java/cn/danatech/cours1/MainActivity.kt
@@ -0,0 +1,15 @@
+package cn.danatech.cours1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.danatech.cours1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
